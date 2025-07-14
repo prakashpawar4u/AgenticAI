1a58c4107bb988892109a7a1f58c632442db9907
Prakashpawar4u <prakashpawar4u@gmail.com>
2025-07-14 12:11:15 +0530

    Adding code for first agent app

---
+++ main.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+
+load_dotenv()
+
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+
+#llm = ChatOpenAI(model="gpt-40-mini")
+#response = llm.invoke("What is the meaning of life")
+# print(response)
+
+
+llm = ChatAnthropic(model="3-5-sonnet-")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
